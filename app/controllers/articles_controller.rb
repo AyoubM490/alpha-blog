c7de286765b25a8b05e7dab94c85be7122ee4b57
AyoubMS <ayoub.chahir96@gmail.com>
2022-08-03 22:09:24 +0100

    add association between user and articles and debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_190611) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_193906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_190611) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
