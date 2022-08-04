119d7a364f750f758eb41cfd4f7aee8da63a7950
AyoubMS <ayoub.chahir96@gmail.com>
2022-08-04 10:31:45 +0100

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles
+
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
   # Defines the root path route ("/")
   # root "articles#index"
 end
